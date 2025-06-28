f8e4df686445e5eef866192f4853cf2c3e5e7e16
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-06-28 16:19:42 +0530

    Refactor app.component.ts and enhance directives.component.ts with new features and improved structure

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,21 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 // import { DataBindingComponent } from '../data-binding/data-binding.component';
 
 @Component({
   selector: 'app-directives',
   imports: [
-    // DataBindingComponent
-    CommonModule
+    // DataBindingComponent    // this is the way to import a component in another component
+    CommonModule,      // CommonModule is imported to use ngIf, ngFor, etc.
+    FormsModule
   ],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.scss'
 })
 export class DirectivesComponent {
   isChecked: boolean = false;
+  isInputField: boolean = true
+
+  inputValue1: string= "";
+  inputValue2: string= "";
 
   onClick() {
     this.isChecked = !this.isChecked;
   }
+
+  show(){
+this.isInputField = true;
+  }
+
+  hide(){
+this.isInputField = false;
+  }
+
+  
 
 }
