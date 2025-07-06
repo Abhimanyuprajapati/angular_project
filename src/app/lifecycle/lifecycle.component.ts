e4ca2005be8e03c203ab60f8e4ece1538e93879a
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-07-06 16:51:48 +0530

    Implement lifecycle hooks in LifecycleComponent for improved functionality

--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './lifecycle.component.html',
   styleUrl: './lifecycle.component.scss'
 })
-export class LifecycleComponent {
+export class LifecycleComponent implements OnChanges, OnInit{
+// constructor is the first life cycle 
+// means it will run first (it will mount first with dom)
+
+constructor (){
+console.log("on mounting constructor called first")
+}
+
+// to implement ngOnChanges() we have to implement on export class classname lifecycle {}
+ngOnChanges(changes: SimpleChanges): void {
+  console.log("1. ngOnChanges is called");
+}
+
+ngOnInit(): void {
+   console.log("2. ngOnInit is called");
+}
 
 }
